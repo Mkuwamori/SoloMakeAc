35b8e0a14b63419d8332ae8ebc113353dcd2d38a
m_Kuwa <midokuwa@gmail.com>
2019-08-21 19:12:51 +0900

    マウスの位置でカメラが動くように カメラがPlayerを追従するように カメラの方向でPlayerが動くように Playerをとりあえずユニティちゃんに

---
+++ New Unity Project/Assets/PlayerMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    float x, z;
+    public Vector3 forward { get; set; }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        transform.LookAt(transform.position + forward);
+        transform.position += ((transform.right * x) + (transform.forward * z)) * 10 * Time.deltaTime;
+    }
+}
