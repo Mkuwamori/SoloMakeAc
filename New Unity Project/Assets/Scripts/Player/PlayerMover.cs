a8d07049b8be1c1760ecbc59aae9f5aff84bf3c4
m_Kuwa <midokuwa@gmail.com>
2019-08-22 18:10:13 +0900

    Unityちゃんの足だけ動くように 浮いたり沈んだりできるように

--- New Unity Project/Assets/Scripts/Player/PlayerMover.cs
+++ New Unity Project/Assets/Scripts/Player/PlayerMover.cs
@@ -7,14 +7,20 @@ public class PlayerMover : MonoBehaviour
     float x, z;
     public Vector3 forward { get; set; }
 
+    Rigidbody rigid;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
         Move();
+        if (Input.GetKey(KeyCode.Space))
+            Float();
+        if (Input.GetKey(KeyCode.LeftShift))
+            Sink();
     }
 
     void Move()
@@ -23,6 +29,15 @@ public class PlayerMover : MonoBehaviour
         z = Input.GetAxis("Vertical");
 
         transform.LookAt(transform.position + forward);
-        transform.position += ((transform.right * x) + (transform.forward * z)) * 10 * Time.deltaTime;
+        rigid.MovePosition(transform.position + ((transform.right * x) + (transform.forward * z)) * 10 * Time.deltaTime);
+    }
+
+    void Float()
+    {
+        rigid.MovePosition(transform.position + Vector3.up * 10 * Time.deltaTime);
+    }
+    void Sink()
+    {
+        rigid.MovePosition(transform.position + Vector3.down * 10 * Time.deltaTime);
     }
 }
